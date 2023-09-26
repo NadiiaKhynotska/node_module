941c9e97ff1f4d6ad026092d9eb109c735de22ba
NadiiaKhynotska <nadinyman@gmail.com>
2023-09-26 23:54:42 +0300

    homework2

--- main.js
+++ main.js
@@ -1,181 +1,50 @@
-// const Events = require("node:events");
-// const eventEmitter = new Events();
-//
-// eventEmitter.on('click', ()=>{
-//     console.log('Click, click, click')
-// })
-//
-// eventEmitter.emit('click')
-// eventEmitter.emit('click')
-// eventEmitter.emit('click')
-// eventEmitter.emit('click')
-//
-// eventEmitter.once('hello', ()=>{
-//     console.log('Hello, hello, hello')
-// })
-// console.log(eventEmitter.eventNames())
-// eventEmitter.emit('hello')
-// eventEmitter.emit('hello')
-// eventEmitter.emit('hello')
-// eventEmitter.emit('hello')
-// console.log(eventEmitter.eventNames())
-
-// const fs = require('fs')
-// const path = require('path')
-// const mainPath = path.join(__dirname, 'folder')
-// const filePath = path.join(mainPath, 'file.txt')
-// // fs.mkdir(mainPath,err => {})
-// // fs.writeFile(filePath, 'text in file', err => {})
-//
-// const readStream = fs.createReadStream(filePath,{highWaterMark: 30})
-// const writeStream = fs.createWriteStream(path.join(mainPath, 'file2.txt')
-// )
-//
-// readStream.on('data', (chunk)=>{
-//     console.log(chunk)
-//     writeStream.write(chunk)
-// })
-//
-// // readStream.pipe(writeStream)
-//
-// readStream.on('error', ()=>{
-//     console.log('error happened');
-//     readStream.destroy()
-//     writeStream.end('Error')
-// })
 
 const express = require('express')
+
+const fsService = require('./fs.service')
 const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-const users = [
-    {
-        id: 1,
-        name: "John Smith",
-        age: 25,
-        gender: "Male",
-        email: "john.smith@example.com"
-    },
-    {
-        id: 2,
-        name: "Jane Doe",
-        age: 30,
-        gender: "Female",
-        email: "jane.doe@example.com"
-    },
-    {
-        id: 3,
-        name: "Michael Johnson",
-        age: 22,
-        gender: "Male",
-        email: "michael.johnson@example.com"
-    },
-    {
-        id: 4,
-        name: "Emily Williams",
-        age: 28,
-        gender: "Female",
-        email: "emily.williams@example.com"
-    },
-    {
-        id: 5,
-        name: "Robert Brown",
-        age: 35,
-        gender: "Male",
-        email: "robert.brown@example.com"
-    },
-    {
-        id: 6,
-        name: "Sarah Taylor",
-        age: 29,
-        gender: "Female",
-        email: "sarah.taylor@example.com"
-    },
-    {
-        id: 7,
-        name: "David Anderson",
-        age: 40,
-        gender: "Male",
-        email: "david.anderson@example.com"
-    },
-    {
-        id: 8,
-        name: "Linda Martinez",
-        age: 26,
-        gender: "Female",
-        email: "linda.martinez@example.com"
-    },
-    {
-        id: 9,
-        name: "William Wilson",
-        age: 32,
-        gender: "Male",
-        email: "william.wilson@example.com"
-    },
-    {
-        id: 10,
-        name: "Olivia Jones",
-        age: 27,
-        gender: "Female",
-        email: "olivia.jones@example.com"
-    },
-    {
-        id: 11,
-        name: "James Davis",
-        age: 24,
-        gender: "Male",
-        email: "james.davis@example.com"
-    },
-    {
-        id: 12,
-        name: "Maria Garcia",
-        age: 31,
-        gender: "Female",
-        email: "maria.garcia@example.com"
-    },
-    {
-        id: 13,
-        name: "Daniel Brown",
-        age: 23,
-        gender: "Male",
-        email: "daniel.brown@example.com"
-    },
-    {
-        id: 14,
-        name: "Ella White",
-        age: 33,
-        gender: "Female",
-        email: "ella.white@example.com"
-    },
-    {
-        id: 15,
-        name: "Thomas Clark",
-        age: 36,
-        gender: "Male",
-        email: "thomas.clark@example.com"
-    }
-]
-
-
-app.get('/users', (req, res) => {
-    res.json({
-        data: users
-    })
+app.get('/users', async (req, res) => {
+    const users = await fsService.reader()
+    res.json(users)
 })
 
-app.get('/users/:id', (req, res) => {
-
+app.get('/users/:id', async (req, res) => {
     const {id} = req.params
-
-    res.json({
-        data: users[+id - 1]
-    })
+    const users = await fsService.reader()
+    const user = users.find(user => user.id === Number(id))
+    res.json(user)
 })
 
-app.post('/users', (req, res) => {
-    users.push(req.body)
-    res.status(201).json({message: 'user created'})
+app.post('/users', async (req, res) => {
+    try {
+        const {name, age, gender, email} = req.body
+        if (!name || name.length <= 2) {
+            throw new Error('incorrect name')
+        }
+        if (!age || age < 4) {
+            throw new Error('invalid age')
+        }
+        if (!gender && gender !== "Female" && gender !== "Male") {
+            throw new Error('invalid value of gender')
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('invalid email')
+        }
+
+        const users = await fsService.reader();
+
+        const lasId = users[users.length - 1].id
+        const newUser = {id: lasId + 1, name, age, gender, email}
+        users.push(newUser)
+        await fsService.writer(users)
+        res.status(201).json(newUser)
+    } catch (e) {
+        res.status(400).json(e.message)
+    }
 })
 
 app.delete('/users/:id',(req, res)=>{
