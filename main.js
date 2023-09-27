fbe69f060a4221b0c0300ca893528f26e9d51023
NadiiaKhynotska <nadinyman@gmail.com>
2023-09-27 09:53:57 +0300

    homework2

--- main.js
+++ main.js
@@ -13,10 +13,19 @@ app.get('/users', async (req, res) => {
 })
 
 app.get('/users/:id', async (req, res) => {
-    const {id} = req.params
-    const users = await fsService.reader()
-    const user = users.find(user => user.id === Number(id))
-    res.json(user)
+    try {
+        const {id} = req.params
+        const users = await fsService.reader()
+
+        const user = users.find(user => user.id === Number(id))
+        if (!user) {
+            throw new Error('user nor found')
+        }
+        res.json(user)
+    } catch (e) {
+        res.status(404).json(e.message)
+    }
+
 })
 
 app.post('/users', async (req, res) => {
@@ -47,21 +56,66 @@ app.post('/users', async (req, res) => {
     }
 })
 
-app.delete('/users/:id',(req, res)=>{
+app.delete('/users/:id', async (req, res) => {
+    try {
     const {id} = req.params;
 
-    users.splice(+id-1, 1)
+        const users = await fsService.reader();
 
-    res.sendStatus(204)
+        const index = users.findIndex(user => user.id === Number(id))
+        if (index === -1) {
+            throw new Error('User not found');
+        }
+        users.splice(index, 1);
+
+        await fsService.writer(users);
+
+        res.sendStatus(204);
+    } catch (e) {
+        res.status(404).json(e.message);
+    }
+
 })
 
-app.put('/users/:id',(req, res)=>{
-    const {id} = req.params;
+app.put('/users/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
 
-    users[id] = res.body;
+        const {name, age, gender, email} = req.body
 
-    res.json({massage: 'user updated'})
+        if (!name || name.length < 2) {
+            throw new Error('incorrect name')
+        }
+        if (!age || age < 4) {
+            throw new Error('invalid age')
+        }
+        if (!gender && gender !== "Female" && gender !== "Male") {
+            throw new Error('invalid value of gender')
+        }
+        if (!email || !email.includes('@')) {
+            throw new Error('invalid email')
+        }
+
+        const users = await fsService.reader();
+        const user = users.find(user => user.id === Number(id))
+
+        if (!user) {
+            throw new Error('User not found')
+        }
+        user.name = name;
+        user.age = age;
+        user.gender = gender;
+        user.email = email;
+
+        await fsService.writer(users)
+        res.status(201).json(user)
+
+    } catch (e) {
+        res.status(400).json(e.message)
+    }
 })
+
+
 const Port = 5001
 app.listen(Port, () => {
     console.log(`server has successfully started on port ${Port}`)
